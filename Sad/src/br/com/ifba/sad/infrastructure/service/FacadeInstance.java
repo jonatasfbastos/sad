7f7caad626f547c5be3f7970fee9ce3b25b6de8a
Cléberson <clebersonrochaads@gmail.com>
2022-10-12 17:05:02 -0300

    Adicionando pacote e classes da fachada

---
+++ Sad/src/br/com/ifba/sad/infrastructure/service/FacadeInstance.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.sad.infrastructure.service;
+
+/**
+ *
+ * @author clebinho
+ */
+
+/* Classe que conterá singleton */
+public class FacadeInstance {
+    
+}
