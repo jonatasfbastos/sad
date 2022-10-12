a7759c7a0067526d780bd8face626c8778c94350
Lucas Bastos <Lucas Bastos@lucas>
2022-10-12 18:45:03 -0300

    Adicionando DAO do usuário

---
+++ Sad/src/br/com/ifba/sad/usuario/dao/IDaoUsuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.sad.usuario.dao;
+
+/**
+ *
+ * @author Lucas Bastos
+ */
+public interface IDaoUsuario {
+    
+}
