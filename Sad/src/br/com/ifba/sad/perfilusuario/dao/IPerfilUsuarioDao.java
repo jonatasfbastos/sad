c4d2235e41de5318025259391c49580fd5bfb1c6
ph715 <ph715@LAPTOP-EQMR77NK>
2022-10-13 10:08:17 -0300

    Adicionando Dao de Perfil de Usuário

---
+++ Sad/src/br/com/ifba/sad/perfilusuario/dao/IPerfilUsuarioDao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.sad.perfilusuario.dao;
+
+/**
+ *
+ * @author ph715
+ */
+public interface IPerfilUsuarioDao {
+    
+}
---
+++ Sad/src/br/com/ifba/sad/perfilusuario/dao/PerfilUsuarioDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.sad.perfilusuario.dao;
+
+import br.com.ifba.sad.infrastructure.basedao.BaseDao;
+
+/**
+ *
+ * @author ph715
+ */
+public class PerfilUsuarioDao extends BaseDao<PerfilUsuario> implements IPerfilUsuarioDao{
+    
+}
