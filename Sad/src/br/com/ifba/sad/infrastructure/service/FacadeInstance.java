096334b20f87968bdc2d23ed944f2e49fbbe1244
Kennedy Souza <fundacaokennedy9a@gmail.com>
2022-10-14 18:18:45 -0300

    Adicionando singleton na fachada

--- Sad/src/br/com/ifba/sad/infrastructure/service/FacadeInstance.java
+++ Sad/src/br/com/ifba/sad/infrastructure/service/FacadeInstance.java
@@ -11,5 +11,34 @@ package br.com.ifba.sad.infrastructure.service;
 
 /* Classe que conterá singleton */
 public class FacadeInstance {
+    /**
+     * Represents the singleton instance of the class.
+     */
+    private static IFacade fachadaInstance;
+
+    /**
+     * Object monitor for synchronize.
+     */
+    private static final Object MONITOR = new Object();
+    
+    /**
+     * Create a new instance of this class.
+     */
+    public FacadeInstance() {
+        super();
+    }
+    
+    /**
+     * Return a new instance of Facade if there's not.
+     * @return 
+     */
+    public static IFacade getInstance() {
+        synchronized (MONITOR) {
+            if (fachadaInstance == null) {
+                fachadaInstance = (IFacade) new Facade();
+            }
+        }
+        return fachadaInstance;
+    }    
     
 }
