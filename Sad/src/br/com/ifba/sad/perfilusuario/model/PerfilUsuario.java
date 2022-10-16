697972be2b88b1eebef352f770da7a511296d013
gislainne <59578050+gislainne@users.noreply.github.com>
2022-10-16 17:36:20 -0200

    criação do model usuario

---
+++ Sad/src/br/com/ifba/sad/perfilusuario/model/PerfilUsuario.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifba.sad.perfilusuario.model;
+
+import br.com.ifba.sad.infrastructure.model.PersistenceEntity;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gislaine Nunes
+ */
+@Entity
+@Table(name = "perfil_usuario")
+public class PerfilUsuario extends PersistenceEntity {
+     
+     private String nome;
+     private String descricao;
+     
+     public PerfilUsuario() {
+     }
+
+     public String getNome() {
+          return nome;
+     }
+
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
+
+     public String getDescricao() {
+          return descricao;
+     }
+
+     public void setDescricao(String descricao) {
+          this.descricao = descricao;
+     }
+     
+}
