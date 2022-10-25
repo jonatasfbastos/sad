afe998524b2d504511f553cf4aea6731a8aab1cf
Kennedy Souza <fundacaokennedy9a@gmail.com>
2022-10-25 15:40:16 -0300

    Adicionando o Dao de curso

---
+++ Sad/src/main/java/br/com/ifba/sad/curso/dao/ICursoDao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.sad.curso.dao;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author kennedy
+ */
+/*
+                Retirar o comentário ao criar o model de curso
+@Repository
+public interface ICursoDao extends JpaRepository<Curso, Long> {
+    public abstract List <Curso> findByName(String name);
+    public abstract List <Curso> findByID(long id);
+}
+*/
