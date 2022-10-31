0c8812c0688254737350bfd0b9c4038278799e05
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2022-10-31 08:44:47 -0300

    Model Curso

---
+++ Sad/src/main/java/br/com/ifba/sad/curso/model/Curso.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.sad.curso.model;
+
+import br.com.ifba.sad.infrastructure.model.PersistenceEntity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author lucas
+ */
+@Entity
+@Table(name = "curso")
+public class Curso extends PersistenceEntity implements Serializable {
+    // Variaveis
+    private String nome;
+    private String codigoCurso;
+    private String sigla;
+    private String Descricao;
+    
+    //construtor
+    public Curso() {
+    }
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the codigoCurso
+     */
+    public String getCodigoCurso() {
+        return codigoCurso;
+    }
+
+    /**
+     * @param codigoCurso the codigoCurso to set
+     */
+    public void setCodigoCurso(String codigoCurso) {
+        this.codigoCurso = codigoCurso;
+    }
+
+    /**
+     * @return the sigla
+     */
+    public String getSigla() {
+        return sigla;
+    }
+
+    /**
+     * @param sigla the sigla to set
+     */
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    /**
+     * @return the Descricao
+     */
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    /**
+     * @param Descricao the Descricao to set
+     */
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+    
+}
