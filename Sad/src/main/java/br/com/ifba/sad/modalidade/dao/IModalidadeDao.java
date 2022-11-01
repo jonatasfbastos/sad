da39d7ddb52a9e087ae309211222b9f8fa0111ab
Kennedy Souza <fundacaokennedy9a@gmail.com>
2022-11-01 16:46:02 -0300

    Adicionando o Dao de modalidade de curso

---
+++ Sad/src/main/java/br/com/ifba/sad/modalidade/dao/IModalidadeDao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.sad.modalidade.dao;
+
+import br.com.ifba.sad.modalidade.model.Modalidade;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author kennedy
+ */
+@Repository
+public interface IModalidadeDao extends JpaRepository<Modalidade, Long> {
+    public abstract List <Modalidade> findByName(String name);
+    public abstract List <Modalidade> findByID(long id);
+}
