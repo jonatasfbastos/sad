a2bf597fe6efb8c5ee747ea876a81b5b2aad8ed8
Jonas Dias <zdias1413@gmail.com>
2022-11-04 10:38:42 -0300

    Adicionando service turma

---
+++ Sad/src/main/java/br/com/ifba/sad/turma/service/IServiceTurma.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.ifba.sad.turma.service;
+
+import java.util.List;
+
+/**
+ *
+ * @author Felipe
+ */
+public interface IServiceTurma {
+    
+    /*
+    Remover quando estiver com o model 
+     // Salva Turma
+    public abstract Turma saveTurma(Turma turma);
+    // Atualiza Turma
+    public abstract Turma updateTurma(Turma turma);
+    // Deleta Turma
+    public abstract void deleteTurma(Turma turma);
+     // Metéodo que retorna todos as Turmas da base de dados
+    public abstract List<Turma> getAllTurma();
+    // Método que retorna a turma com o id informado
+    public Turma findById(Long id);
+    // Método que retorna a lista com as turmas com o nome informado
+    public List<Turma> findByNome(String nome);
+    */
+}
