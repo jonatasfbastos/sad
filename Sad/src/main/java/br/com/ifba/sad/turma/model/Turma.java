33f2f877b55760c764cc6a420c3105b9f7ad7811
limabarreto <107010072+limabarreto@users.noreply.github.com>
2022-11-04 16:28:43 -0300

    prototipo model de turma

--- Sad/src/main/java/br/com/ifba/sad/turma/model/Turma.java
+++ Sad/src/main/java/br/com/ifba/sad/turma/model/Turma.java
@@ -4,10 +4,55 @@
  */
 package br.com.ifba.sad.turma.model;
 
+import br.com.ifba.sad.infrastructure.model.PersistenceEntity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
 /**
  *
- * @author MATHEUS LIMA
+ * @author Gislaine Nunes
  */
-public class Turma {
+@Entity
+@Table(name = "turma")
+public class Turma extends PersistenceEntity implements Serializable {
+    
+    private String nome;
+    private String codigoTurma;
+    private String sigla;
+    private String ativa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigoTurma() {
+        return codigoTurma;
+    }
+
+    public void setCodigoTurma(String codigoTurma) {
+        this.codigoTurma = codigoTurma;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(String ativa) {
+        this.ativa = ativa;
+    }
     
 }
