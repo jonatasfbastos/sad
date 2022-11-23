53c6a6c775a60b138df237e2a080adce3e104ea8
maaix <maxgabriel.santo@hotmail.com>
2022-11-23 14:13:51 -0300

    adicao do service de tipo turma

--- Sad/src/main/java/br/com/ifba/tipoturma/model/TipoTurma.java
+++ Sad/src/main/java/br/com/ifba/tipoturma/model/TipoTurma.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package br.com.ifba.tipoturma.model;
+import br.com.ifba.infrastructure.model.PersistenceEntity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.Data;
 
 /**
  *
- * @author MATHEUS LIMA
+ * @author Gislaine Nunes
  */
-public class TipoTurma {
+@Entity
+@Table(name = "tipoturma")
+@Data
+public class TipoTurma extends PersistenceEntity implements Serializable {
     
+    private String nome;
+    private String descricao;
+
 }
