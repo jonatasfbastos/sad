b9c1166bd253283247a86ec556eb1d5a56618c5c
gislainne <gislainenunes102@gmail.com>
2022-11-29 16:28:03 -0300

    model de terceirizado

---
+++ Sad/src/main/java/br/com/ifba/terceirizado/model/Terceirizado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.terceirizado.model;
+import br.com.ifba.pessoa.model.Pessoa;
+import lombok.Data;
+
+/**
+ *
+ * @author gislaine nunes
+ */
+
+@Data
+public abstract class Tereceirizado extends Pessoa{
+
+        
+}
